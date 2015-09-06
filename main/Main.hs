c50593fc166210f0c1dae0c4639c357ae2c8ed94
Rodrigo B. de Oliveira <rbo@acm.org>
2015-09-06 18:08:59 -0300

    rename codegen executable following Idris master
    > Avoid confusion between the compiler and codegens

--- main/Main.hs
+++ main/Main.hs
@@ -11,7 +11,8 @@ import Idris.REPL
 import System.Environment
 import System.Exit
 
-data Opts = Opts { inputs :: [FilePath]
+data Opts = Opts { really :: Bool
+                 , inputs :: [FilePath]
                  , output :: FilePath }
 
 main :: IO ()
@@ -19,17 +20,24 @@ main = do
   opts <- getOpts
   if null (inputs opts)
     then showUsage
-    else runMain (cilMain opts)
+    else if not $ really opts
+           then do putStrLn "This code generator is intended to be called by the Idris compiler. \
+                            \Please pass Idris the '--codegen' flag to choose a backend."
+                   exitSuccess
+           else runMain (cilMain opts)
 
 getOpts :: IO Opts
-getOpts = liftM (process (Opts [] "a.il")) getArgs
-  where process opts ("-o":o:xs) = process (opts { output = o }) xs
-        process opts (x:xs)      = process (opts { inputs = x:inputs opts }) xs
-        process opts []          = opts
+getOpts = liftM (process (Opts False [] "a.il")) getArgs
+  where process opts ("--yes-really":xs) = process (opts { really = True }) xs
+        process opts ("-o":o:xs)         = process (opts { output = o }) xs
+        process opts (x:xs)              = process (opts { inputs = x:inputs opts }) xs
+        process opts []                  = opts
 
 showUsage :: IO ()
 showUsage = do
-  putStrLn "Usage: idris-cil <ibc-files> [-o <output-file>]"
+  putStrLn "CIL code generator which is intended to be called by the compiler, not by a user."
+  putStrLn "To call the code generator manually, pass the --yes-really option.\n"
+  putStrLn "Usage: idris-codegen-cil --yes-really <ibc-files> [-o <output-file>]"
   exitSuccess
 
 codegenInfoFrom :: Opts -> Idris CodegenInfo
